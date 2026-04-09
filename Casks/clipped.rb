3d37454a8c1143379dc4bd802e3d8ce6333f78bd
Max Clayton Clowes <max@mcclowes.com>
2026-04-09 09:10:18 +0000

    Update clipped to v0.1.5

--- Casks/clipped.rb
+++ Casks/clipped.rb
@@ -1,10 +1,12 @@
 cask "clipped" do
-  version "v0.1.2"
-  sha256 "6900742c65cd1215f8c9812c2a506a439c2f17fb7394a63d355e3df8ab6899c3"
-  url "https://github.com/mcclowes/clipped/releases/download/v0.1.2/Clipped.zip"
+  version "v0.1.5"
+  sha256 "96771c8d10157d14f0e9c55527546565509809ad93621969714d1df7480ea0ca"
+  url "https://github.com/mcclowes/clipped/releases/download/v0.1.5/Clipped.zip"
   name "Clipped"
   desc "Native macOS clipboard manager"
   homepage "https://github.com/mcclowes/clipped"
+
+  depends_on macos: ">= :sequoia"
 
   app "Clipped.app"
 
